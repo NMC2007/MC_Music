33960d04ce6a3b9ee1526e44b60c40d99c50d4b5
NMC2007 <nmc281207@gmail.com>
2026-08-21 22:10:53 +0700

    docker setup

---
+++ database/init-docker.sql
@@ -0,0 +1,4 @@
+create schema if not exists mc_music_users_schema;
+create schema if not exists mc_music_artists_schema;
+create schema if not exists mc_music_admin_schema;
+create schema if not exists mc_music_catalog_schema;
